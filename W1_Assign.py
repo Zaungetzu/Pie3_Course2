d020d659300cdab4b5c8363cd024c006fe0831c5
Zaungetzu <130801091+Zaungetzu@users.noreply.github.com>
2023-07-25 10:59:15 -0600

    Create W1_Assign.py

---
+++ W1_Assign.py
@@ -0,0 +1,79 @@
+#The textfile, travel_plans.txt, contains the summer travel plans for someone with some commentary. 
+#Find the total number of characters in the file and save to the variable num.
+
+T_plans = open("travel_plans.txt", 'r')
+char = T_plans.read()	
+num=len(char)
+print(char)
+
+
+
+#We have provided a file called emotion_words.txt that contains lines of words that describe emotions. 
+#Find the total number of words in the file and assign this value to the variable num_words.
+
+Emo_wrd = open("emotion_words.txt", 'r')
+num_words = 0
+char = Emo_wrd.read()
+wrd = char.replace("\n", " ")									##txt = "I like bananas"
+wrd_list = wrd.split()										  	##x = txt.replace("bananas", "apples")
+num_words=len(wrd_list)										  	##print(x)
+print(num_words)
+##deleting hidden \n for listing##
+
+
+#Assign to the variable num_lines the number of lines in the file school_prompt.txt.
+
+fileref = open("school_prompt.txt", "r")
+lines = fileref.readlines()	
+num_lines=len(lines)
+print(num_lines)
+
+
+# Assign the first 30 characters of school_prompt.txt as a string to the variable beginning_chars.
+
+fileref = open("school_prompt.txt", "r")
+lines = fileref.read()	
+beginning_chars = lines[:30]
+print(beginning_chars)
+
+
+# Challenge: Using the file school_prompt.txt, assign the third word of every line to a list called three.
+
+fileref = open("school_prompt.txt", "r")
+line = fileref.readlines()
+three = []
+for words in line:
+    word = words.split()
+    three.append(word[2])
+print(three)
+
+
+#Challenge: Create a list called emotions that contains the first word of every line in emotion_words.txt.
+
+fileref = open("emotion_words.txt", "r")
+line = fileref.readlines()
+emotions = []
+for words in line:
+    word = words.split()
+    emotions.append(word[0])
+print(emotions)
+
+
+#Assign the first 33 characters from the textfile, travel_plans.txt to the variable first_chars.
+
+fileref = open("travel_plans.txt", "r")
+lines = fileref.read()	
+first_chars = lines[:33]
+print(first_chars)
+
+#Challenge: Using the file school_prompt.txt, if the character ‘p’ is in a word, then add the word to a list called p_words.
+
+fileref = open("school_prompt.txt", "r")
+lines = fileref.read()
+p_words = []
+wrds = lines.split()
+for i in wrds:
+    if "p" in i:
+        p_words.append(i)
+print(p_words)
+
